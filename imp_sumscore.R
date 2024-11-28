eac5791fef53d2127fae461ee0653429e2b59fa2
htzheng1018 <htzheng@umich.edu>
2024-11-27 22:10:11 -0500

    sumscore

---
+++ imp_sumscore.R
@@ -0,0 +1,26 @@
+# simple pmm imputation
+imp_pmm_sumscore = function(data, tv, iv, x_sum, m) {
+  # imputation dataset
+  data_use = data %>%
+    dplyr::select(id, year, tv, iv, y)
+  data_use$id = as.integer(data_use$id)
+  data_use = data_use %>%
+    mutate_all(~ifelse(. == "NaN", NA, .))
+  
+  # set imputation parameters
+  ini = mice(data_use, maxit = 0)
+  
+  pred = ini$pred
+  pred[tv, x_sum] = 0
+  pred[iv, tv] = 0
+  pred[x_sum, ] = 0
+  pred[y, tv] = 0
+  
+  meth = ini$meth
+  meth[x_sum] = sprintf("~I(%s + %s + %s)", tv[1], tv[2], tv[3])
+  
+  # imputation process
+  imp.use = mice(data_use, pred = pred, meth = meth, m = m, maxit = 5, seed = 1018, printFlag = F)
+  
+  return(imp.use)
+}
--- try.R
+++ try.R
@@ -8,5 +8,14 @@
 
 
 
-mydata = create_data(100, 2000, "Normal", c(4, 5, 2, 1), 0.7, "Exponential", c(log(sqrt(3.2)), log(sqrt(5/4))), 3)$data
+mydata = create_data(100, 2000, "Normal", c(4, 5, 2, 1), c(0.7, 0.7, 0.7), "Exponential", c(log(sqrt(3.2)), log(sqrt(5/4))), 3)$data
 # mydata_raw = create_data(100, 2000, "Poisson", 8, 2, "Gamma", c(3, 2), 12)$raw
+
+
+mean(mydata$x1, na.rm = T)
+mean(mydata$x2, na.rm = T)
+mean(mydata$x3, na.rm = T)
+
+# imputation process
+m = 5
+imp.use2l = imp_2lpmm_sumscore(mydata, c("x1", "x2", "x3"), "z1", m)
